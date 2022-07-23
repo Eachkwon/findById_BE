6ad8f56597c3b6904624d6b6556b6a5fd5409a46
Park-Tae_Woong <ptw0428@naver.com>
2022-07-23 20:01:28 +0900

    컨트롤러 작성, DTO 추가, 서비스 추가

--- src/main/java/com/example/week06/Dto/CommentResponseDto.java
+++ src/main/java/com/example/week06/Dto/CommentResponseDto.java
@@ -1,4 +1,12 @@
 package com.example.week06.Dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class CommentResponseDto {
+    private String nickname;
+    private String comment;
+    private String createdAt;
 }
--- src/main/java/com/example/week06/controller/CommentController.java
+++ src/main/java/com/example/week06/controller/CommentController.java
@@ -1,4 +1,25 @@
 package com.example.week06.controller;
 
+import com.example.week06.Dto.CommentRequestDto;
+import com.example.week06.Dto.CommentResponseDto;
+import com.example.week06.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
 public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping("/api/posts/{postId}/comments")
+    public void createComment(@RequestBody CommentRequestDto commentRequestDto, @PathVariable Long postId){
+        commentService.createComment(commentRequestDto, postId);
+    }
+
+    @GetMapping("/api/posts/{postId}/comments")
+    public CommentRequestDto getComment(@PathVariable Long restaurantId){
+        return commentService.getComment(restaurantId);
+    }
+
+
 }
