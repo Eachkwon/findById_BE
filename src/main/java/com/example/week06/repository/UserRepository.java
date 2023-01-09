4981f76bed3e937c32e5b8faac3730379fb27180
Eachkwon <eachkwon@naver.com>
2023-01-09 23:15:00 +0900

    modify password logic

--- src/main/java/com/example/week06/repository/UserRepository.java
+++ src/main/java/com/example/week06/repository/UserRepository.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findById(Long userId);
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
     Boolean existsByNickname(String nickname);
