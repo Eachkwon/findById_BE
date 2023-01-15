094813d8ff956d40cd869a2b1579af79bdae4ede
eachkwon@naver.com <eachkwon@naver.com>
2023-01-15 22:43:24 +0900

    code refactoring: community entity

--- src/main/java/com/example/week06/domain/community/entity/Comment.java
+++ src/main/java/com/example/week06/domain/community/entity/Comment.java
@@ -4,14 +4,12 @@ package com.example.week06.domain.community.entity;
 import com.example.week06.domain.community.dto.CommentRequestDto;
 import com.example.week06.global.Timestamped;
 import com.example.week06.domain.user.entity.User;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Entity
 public class Comment extends Timestamped {
@@ -20,15 +18,12 @@ public class Comment extends Timestamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String comment;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne
-    @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
     public Comment(CommentRequestDto commentRequestDto, User user, Post post){
