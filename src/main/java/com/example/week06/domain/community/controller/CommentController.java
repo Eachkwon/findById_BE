952053c647999a679539afa073b5f18a2b19d448
eachkwon@naver.com <eachkwon@naver.com>
2023-01-24 20:50:53 +0900

    Modify: add success messages in comment methods

--- src/main/java/com/example/week06/domain/community/controller/CommentController.java
+++ src/main/java/com/example/week06/domain/community/controller/CommentController.java
@@ -1,9 +1,12 @@
 package com.example.week06.domain.community.controller;
 
 
+import com.example.week06.global.common.SuccessResponse;
 import com.example.week06.global.security.UserDetailsImpl;
 import com.example.week06.domain.community.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,18 +19,18 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/api/posts/{postId}/comments")
-    public void createComment(@RequestBody Map<String, String> map, @PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        commentService.createComment(map.get("content"), postId, userDetails.getUser());
+    public ResponseEntity<?> createComment(@RequestBody Map<String, String> map, @PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return ResponseEntity.ok(new SuccessResponse<>(HttpStatus.OK, commentService.createComment(map.get("content"), postId, userDetails.getUser())));
     }
 
     @DeleteMapping("/api/comments/{commentId}")
-    public void deleteComment(@PathVariable Long commentId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        commentService.deleteComment(commentId, userDetails.getUser());
+    public ResponseEntity<?> deleteComment(@PathVariable Long commentId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok(new SuccessResponse<>(HttpStatus.OK, commentService.deleteComment(commentId, userDetails.getUser())));
     }
 
     @PutMapping("/api/comments/{commentId}")
-    public void putComment(@PathVariable Long commentId, @RequestBody Map<String, String> map, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        commentService.putComment(commentId, map.get("content"), userDetails.getUser());
+    public ResponseEntity<?> putComment(@PathVariable Long commentId, @RequestBody Map<String, String> map, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return ResponseEntity.ok(new SuccessResponse<>(HttpStatus.OK, commentService.putComment(commentId, map.get("content"), userDetails.getUser())));
     }
 
 
